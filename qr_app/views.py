1d1b1ed4ea18751915ce7a1f2ba3647a184956b1
rinas <topi2frie3elec@gmail.com>
2022-08-24 21:27:55 +0900

    hello world

--- qr_app/views.py
+++ qr_app/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
+import json
+
+# param = json.loads(request.data.decode('utf-8'))
+
 
 def index(request):
-    return render(request, 'tmp/index.html')
+    return render(request, 'index.html')
 
 # Create your views here.
